7f714cfcdc295267d7e2bb5f05229dc743b20b97
Abhishek Choudhary <abhishekchoudhary38808@gmail.com>
2025-11-07 22:44:47 +0530

    Fix Allowed_hosts for render

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3cc7^*mza7u*51cztbaur7)s10!6jkk)j&q2y^tw#%-yq+p%&j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["chat-backend-ec08.onrender.com", "localhost", "127.0.0.1"]
 
